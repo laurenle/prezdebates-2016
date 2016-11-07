e031e32ca35805c5af3e59204cca256df9af7c18
Lauren <laleung94@gmail.com>
2016-11-07 18:45:54 -0500

    added sentence by sentence parsing

--- analyze.py
+++ analyze.py
@@ -1,4 +1,5 @@
 import re, nltk, math
+from nltk.corpus import treebank
 
 # List of text files to be analyzed
 filenames = ['debate1.txt', 'debate2.txt', 'debate3.txt']
@@ -18,6 +19,8 @@ def variance(numbers):
 def std_dev(numbers):
   return math.sqrt(variance(numbers))
 
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
 # main loop. iteratively parse each file
 for filename in filenames:
   f = open(filename, 'r')
@@ -29,24 +32,26 @@ for filename in filenames:
   for line in f:
     speaker = re.match( r'[A-Z]+\:', line)
     if speaker:
-      if current_speaker in speech_segments:
-        speech_segments[current_speaker].append(current_segment)
-      elif current_speaker != '':
-        speech_segments[current_speaker] = [current_segment]
       current_speaker = speaker.group(0)[:-1].lower()
       current_segment = line.decode('utf-8').split(': ', 1)[1].strip()
     else:
       current_segment += line.decode('utf-8').strip()
+    sentences = tokenizer.tokenize(current_segment)
 
+    for sentence in sentences:
+      if current_speaker in speech_segments:
+        speech_segments[current_speaker].append(sentence)
+      else:
+        speech_segments[current_speaker] = [sentence]
   f.close()
 
 for speaker in speech_segments:
-  print "Parsed {0} segments for speaker {1}".format(len(speech_segments[speaker]), speaker)
+  print "Parsed {0} sentences for speaker {1}".format(len(speech_segments[speaker]), speaker)
   segment_lengths = []
   for segment in speech_segments[speaker]:
     tokens = nltk.word_tokenize(segment)
     segment_lengths.append(len(tokens))
-  print "Speaker {0} averaged at {1} words per segment, with a min of {2}, a max of {3}, and a standard deviation of {4}\n".format(
+  print "Speaker {0} averaged at {1} words per sentence, with a min of {2}, a max of {3}, and a standard deviation of {4}\n".format(
     speaker, mean(segment_lengths), min(segment_lengths), max(segment_lengths), std_dev(segment_lengths))
 
 
