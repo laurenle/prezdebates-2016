7328b2eb544ed47303bfd4c62f0f498f035d1b04
Lauren <laleung94@gmail.com>
2016-11-06 23:14:34 -0500

    Updated analyze py to use UTF 8, updated readme, updated typo in transcript

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # prezdebates-2016
 Comparative speech analysis for candidates Hillary Rodham Clinton and Donald J. Trump using data drawn from the 2016 presidential debates.
+
+## Installation
+
+Before running analyze.py, you will need to install the nltk module:
+
+    $ sudo pip install -U nltk
+    $ sudo pip install -U numpy
+
+Once this is complete, you can run the script using:
+
+    $ python analyze.py
+
+For some reason, you may need to download additional nltk modules using the built-in installer. To do so, the following two commands on terminal:
+
+    $ python
+    >>> import nltk
+    >>> nltk.download()
+
+A NLTK Downloader GUI will open. From here, select "All packages" and click the Download button.
+
+## Notes
+
+- Data for Middle-Eastern Americans, Asian Americans, and Native Americans was not included because these groups were not mentioned by either candidate.
+- Data for 'Mexicans' was not included in the search for Latinos/Hispanics, because whether this referred to Mexican-Americans or Mexican citizens was ambiguous.
--- analyze.py
+++ analyze.py
@@ -1,4 +1,5 @@
-import re, json
+import re, json, nltk
+from nltk.corpus import treebank
 
 # List of text files to be analyzed
 filenames = ['debate1.txt', 'debate2.txt', 'debate3.txt']
@@ -18,11 +19,13 @@ for filename in filenames:
     speaker = re.match( r'[A-Z]+\:', line)
     mentions_black = re.search( r'((A|a)frican( *)(-*)( *)(A|a)merican)|((B|b)lack)', line)
     mentions_latino = re.search( r'((L|l)atin(o|a))|((H|h)ispanic)', line)
+    # if a speaker is introduced, update the current speaker accordingly
     if speaker:
       current_speaker = speaker.group(0)[:-1].lower()
-      current_segment = line.split(': ', 1)[1].strip()
+      current_segment = line.decode('utf-8').split(': ', 1)[1].strip()
     else:
-      current_segment = line
+      current_segment = line.decode('utf-8').strip()
+    # if african americans or latinos are mentioned, add speech segment for analysis
     if (mentions_black or mentions_latino) and current_speaker in speech_segments:
       speech_segments[current_speaker].append(current_segment)
     elif mentions_black or mentions_latino:
@@ -33,3 +36,7 @@ for speaker in speech_segments:
   print "Parsed {0} lines for speaker '{1}'".format(len(speech_segments[speaker]), speaker)
   for segment in speech_segments[speaker]:
     print segment
+    tokens = nltk.word_tokenize(segment)
+    tagged = nltk.pos_tag(tokens)
+    entities = nltk.chunk.ne_chunk(tagged)
+    # print entities
