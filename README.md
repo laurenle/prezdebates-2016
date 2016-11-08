6b4cdee911e96c6add708a229a13f740600feb71
Lauren <laleung94@gmail.com>
2016-11-07 19:30:58 -0500

    Program now provides total number of word occurences

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # prezdebates-2016
-Comparative speech analysis for candidates Hillary Rodham Clinton and Donald J. Trump using data drawn from the 2016 presidential debates.
+Comparative speech analysis of usage for "Hispanic/Latin@" and "African-American/black" word choice for candidates Hillary Rodham Clinton and Donald J. Trump using data drawn from the 2016 presidential debates.
 
 ## Installation
 
@@ -19,8 +19,3 @@ For some reason, you may need to download additional nltk modules using the buil
     >>> nltk.download()
 
 A NLTK Downloader GUI will open. From here, select "All packages" and click the Download button.
-
-## Notes
-
-- Data for Middle-Eastern Americans, Asian Americans, and Native Americans was not included because these groups were not mentioned by either candidate. Additional research may explore other minority groups over a wider range of data.
-- Data for 'Mexicans' was not included in the search for Latinos/Hispanics, because whether this referred to Mexican-Americans or Mexican citizens was ambiguous.
--- analyze.py
+++ analyze.py
@@ -4,7 +4,7 @@ from nltk.corpus import treebank
 # List of text files to be analyzed
 filenames = ['debate1.txt', 'debate2.txt', 'debate3.txt']
 
-speech_segments = {'clinton': [], 'trump': []}
+minority_mention_count = {}
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
@@ -23,20 +23,22 @@ for filename in filenames:
       current_segment = line.decode('utf-8').split(': ', 1)[1].strip()
     else:
       current_segment = line.decode('utf-8').strip()
-    # if african americans or latinos are mentioned, add speech segment for analysis
+    if current_speaker not in minority_mention_count:
+      minority_mention_count[current_speaker] = {'black': 0, 'hispanic': 0, 'latino': 0, 'african-american': 0}
     sentences = tokenizer.tokenize(current_segment)
 
     for sentence in sentences:
-      mentions_black = re.search( r'((A|a)frican( *)(-*)( *)(A|a)merican)|((B|b)lack)', sentence)
-      mentions_latino = re.search( r'((L|l)atin(o|a))|((H|h)ispanic)', sentence)
-      if (mentions_black or mentions_latino) and current_speaker in speech_segments:
-        speech_segments[current_speaker].append(sentence)
-      elif mentions_black or mentions_latino:
-        speech_segments[current_speaker] = [sentence]
+      if re.search( r'(B|b)lack', sentence):
+        minority_mention_count[current_speaker]['black'] += 1
+      if re.search( r'(A|a)frican( *)(-*)( *)(A|a)merican', sentence):
+        minority_mention_count[current_speaker]['african-american'] += 1
+      if re.search( r'(L|l)atin(o|a)', sentence):
+        minority_mention_count[current_speaker]['latino'] += 1
+      if re.search( r'(H|h)ispanic', sentence):
+        minority_mention_count[current_speaker]['hispanic'] += 1
   f.close()
 
-for speaker in speech_segments:
+for speaker in minority_mention_count:
   print "-----------------------------------------"
-  print "Parsed {0} lines for speaker '{1}'".format(len(speech_segments[speaker]), speaker)
-  for segment in speech_segments[speaker]:
-    print segment
+  for minority in minority_mention_count[speaker]:
+    print "Speaker {0} used term {1} {2} times".format(speaker, minority, minority_mention_count[speaker][minority])
